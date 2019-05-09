deaf8673fe6138c000c1791d446db3898f810a54
deewaiz <deewaiz@yandex.ru>
2019-05-09 14:30:45 +1100

    posts

--- JSONPlaceholder/ViewController.swift
+++ JSONPlaceholder/ViewController.swift
@@ -12,9 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("start")
+        let session = URLSession.shared
+        //let url = URL(string: "https://learnappmaking.com/ex/users.json")!
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            if error != nil || data == nil {
+                print("Client error!")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                print("Server error!")
+                return
+            }
+            
+            guard let mime = response.mimeType, mime == "application/json" else {
+                print("Wrong MIME type!")
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print(json)
+            } catch {
+                print("JSON error: \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
     }
-
-
 }
 
